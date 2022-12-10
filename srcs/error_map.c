3fc239083f69063f55ff41818555ddc5702c9e5a
mkhellou <yoyomcawom@gmail.com>
2022-12-10 11:39:01 +0100

    better code 2

--- srcs/error_map.c
+++ srcs/error_map.c
@@ -6,13 +6,13 @@
 /*   By: mkhellou < mkhellou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 09:43:06 by mkhellou          #+#    #+#             */
-/*   Updated: 2022/12/10 10:28:20 by mkhellou         ###   ########.fr       */
+/*   Updated: 2022/12/10 11:38:32 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void error_exit_function(map_check *check)
+void	error_exit_function(map_check *check)
 {
 	ft_printf("Error\n");
 	ft_printf("Invalid map :\n");
@@ -21,7 +21,7 @@ void error_exit_function(map_check *check)
 	if (check->stranger_characters == -1)
 		ft_printf("- map has a stranger character\n");
 	if (check->minimum_characters == -1)
-		ft_printf("- map does not respect the number possible of coins , exit and starting point\n");
+		ft_printf("- wronge number of coins ,exit and start\n");
 	if (check->closed == -1)
 		ft_printf("- the map is not closed by walls\n");
 	if (check->with_exit == -1)
@@ -31,38 +31,42 @@ void error_exit_function(map_check *check)
 	exit(EXIT_FAILURE);
 }
 
-void file_name_checker(char *str)
+void	file_name_checker(char *str)
 {
-	int error;
-	int fd;
+	int		error;
+	int		fd;
+	int		len;
+	char	*ptr;
+
 	error = 0;
-	fd = open(str,O_RDONLY);
-	if (fd == -1)
-		error = -1;
-	if (ft_strnstr(str,".ber",ft_strlen(str)) == 0)
-		error = -1;
-	else if (*(ft_strrchr(str,'.')+4) != '\0')
-	 	error = -1;
-	else if (str[0] == *ft_strnstr(str,".ber",ft_strlen(str)) && str[1] == *(ft_strnstr(str,".ber",ft_strlen(str))+1) && str[2] == *(ft_strnstr(str,".ber",ft_strlen(str))+2))
-		error = -1;
-	if (error == -1)
-	{
-		ft_printf("Error\nInvalid file extention or unixisting file\n");
+	len = 0;
+	if (ft_strlen(str) > 4)
+		len = ft_strlen(str) - 4;
+	ptr = str + len ;
+	if (ft_strncmp(".ber", ptr, 4) != 0 || ft_strlen(str) < 5)
+	{	
+		ft_printf("Error\nInvalid file extention\n");
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		ft_printf("Valid file name\n");
-		close(fd);
+		fd = open(str, O_RDONLY);
+		if (fd == -1)
+		{
+			ft_printf("Error\nUnable to open file\n");
+			exit(EXIT_FAILURE);
+		}
 	}
+	ft_printf("Valid file name\n");
+	close(fd);
 }
 
-void check_cordonates(char **map,pos *cordonates)
+void	check_cordonates(char **map, pos *cordonates)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	get_dimentions(map,cordonates);
+	get_dimentions(map, cordonates);
 	if (cordonates->x * SPRITE_X > WINDOW_X)
 	{
 		i = 1;
@@ -82,9 +86,9 @@ void check_cordonates(char **map,pos *cordonates)
 		ft_printf("Size screen is valid\n");
 }
 
-map_info map_checker(int av,char **ac)
+map_info	map_checker(int av, char **ac)
 {
-	map_info map;
+	map_info	map;
 
 	if (av != 2)
 	{
@@ -97,6 +101,6 @@ map_info map_checker(int av,char **ac)
 		exit(EXIT_FAILURE);
 	if (!map_error_handling(map.map))
 		exit(EXIT_FAILURE);
-	check_cordonates(map.map,&map.resolution);
-	return(map);
+	check_cordonates(map.map, &map.resolution);
+	return (map);
 }
